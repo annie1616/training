76b1a89fc1757f7285a835aad2fa253ae1e327d0
unknown <cogjava4346@iiht.tech>
2022-11-08 18:02:53 +0530

    Exception -> throw

---
+++ src/com/exception/Eligibility.java
@@ -0,0 +1,24 @@
+package com.exception;
+
+public class Eligibility {
+	
+	public static void Eligible(Integer exp)
+	{
+		if(exp>=10) {
+			throw new ArithmeticException(">= 10 years. throw is used to send user-defined exceptions");
+		
+	}else
+	{
+		System.out.println("< 10 years");
+		
+	}
+
+}
+	
+	public static void main(String[] args) {
+		Eligible(8);
+	}
+	
+}
+
+
